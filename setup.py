33a59e9b3c24bf738c610db48b732fa6dfd59073
Loïc Carr <lcarr@online.net>
2017-11-17 12:08:51 +0100

    Add compatibilities to installation process

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['pbr'],
+    install_requires=[
+        'configparser==3.5.0',
+        'future==0.16.0',
+        'six==1.11.0',
+    ],
     pbr=True
 )
