eab70d48734b6afbeb3c35e2e22d8d9f8c80be09
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-12-01 10:55:00 +0100

    Do not fail pkg-config version test on master branch

--- tools/oio-check-version.sh
+++ tools/oio-check-version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 # This will work in the main repository, but not in forks
 LATEST_TAG=$(git fetch --tags && git describe --tags --first-parent)
 if [ -z "$LATEST_TAG" ]
@@ -16,14 +18,19 @@ PKG_CONFIG_PATH=/tmp/oio/lib/pkgconfig
 export PKG_CONFIG_PATH
 PKGCONFIG_VERSION=$(pkg-config --modversion oio-sds)
 
-echo "oio-sds short version is           $SHORT_VERSION"
+echo "oio-sds version from tag is        $SHORT_VERSION"
 echo "oio-sds version from pkg-config is $PKGCONFIG_VERSION"
 
 # Ensure pkg-config version is up-to-date
-if [ "$SHORT_VERSION" == "$PKGCONFIG_VERSION" ]
+if [ "$SHORT_VERSION" = "$PKGCONFIG_VERSION" ]
 then
   echo "OK"
 else
+  if [ "${CURRENT_BRANCH}" = "master" ]
+  then
+    echo "Ignoring version mismatch as we are on master branch."
+    exit 0
+  fi
   echo "KO"
   exit 1
 fi
